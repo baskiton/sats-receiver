09a17fc63d0e104204add1151e004c50258da5c6
baskiton <baskiton@mail.ru>
2023-01-18 21:44:30 +0700

    versioning changed to git tag-based schema

--- sats_receiver/__init__.py
+++ sats_receiver/__init__.py
@@ -1,9 +1,6 @@
 import pathlib
 
 
-__version__ = '0.1.0'
-
-
 HOMEDIR = pathlib.Path('~/sats_receiver').expanduser()
 LOGSDIR = HOMEDIR / 'logs'
 TLEDIR = HOMEDIR / 'tle'
--- setup.py
+++ setup.py
@@ -1,31 +1,21 @@
-import pathlib
 import sys
 
 from setuptools import setup, find_packages
 
 
 # Check python version
-MINIMAL_PY_VERSION = (3, 7)
+MINIMAL_PY_VERSION = (3, 8)
 if sys.version_info < MINIMAL_PY_VERSION:
     raise RuntimeError('This app works only with Python {}+'.format('.'.join(map(str, MINIMAL_PY_VERSION))))
 
-HERE = pathlib.Path(__file__).parent
-
-
-def get_version():
-    for line in (HERE / 'sats_receiver' / '__init__.py').open():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-
-
-def get_long_description():
-    return (HERE / 'README.md').read_text('utf-8')
-
-
 setup(
     name='sats_receiver',
-    version=get_version(),
+    setuptools_git_versioning={
+        'enabled': True,
+        'template': '{tag}.{ccount}',
+        'dev_template': '{tag}.{ccount}',
+        'dirty_template': '{tag}.{ccount}',
+    },
     url='https://github.com/baskiton/sats-receiver',
     project_urls={
         'Bug Tracker': "https://github.com/baskiton/sats-receiver/issues",
@@ -35,7 +25,7 @@ setup(
     python_requires='>=3.7',
     author_email='baskiton@gmail.com',
     description='Satellites data receiver based on GNU Radio',
-    long_description=get_long_description(),
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
@@ -45,6 +35,9 @@ setup(
         'pyshp',
         'python-dateutil',
         'scipy',
+    ],
+    setup_requires=[
+        'setuptools-git-versioning',
     ],
     classifiers=[
         'License :: OSI Approved :: MIT License',
