9878320616ab320344ccb9b20ee3c67e68d4dce1
baskiton <baskiton@mail.ru>
2023-08-05 18:14:39 +0700

    add examples for NOAA-APT

--- example/apt.py
+++ example/apt.py
@@ -59,3 +59,67 @@ def apt_to_png_map(apt_filename: Union[str, pathlib.Path], config: Union[pathlib
     img.save(ret_fn, 'png')
 
     return ret_fn
+
+
+def apt_to_composites(apt_filename: Union[str, pathlib.Path]) -> pathlib.Path:
+    """
+    Create composite images based on WxToImg LUT's
+    :param apt_filename: path to APT file
+    :return: Result directory path
+    """
+    apt_filename = pathlib.Path(apt_filename)
+    result_dir = apt_filename.parent / apt_filename.stem
+    result_dir.mkdir(parents=True, exist_ok=True)
+    ret_fn = result_dir / (apt_filename.stem + '.png')
+
+    apt = Apt.from_apt(apt_filename)
+
+    if apt.process():
+        sys.exit(1)
+
+    comps = 'HVC', 'HVCT', 'MSA', 'NO', 'HVCP', 'HVCTP', 'MSAP', 'SEA', 'THRM', 'BP'
+    for c, img in apt.create_composites(*comps):
+        img.save(ret_fn.with_stem(f'{ret_fn.stem}_{c}'), 'png')
+
+    return result_dir
+
+
+def apt_to_images(apt_filename: Union[str, pathlib.Path], config: Union[pathlib.Path, str, Mapping]) -> pathlib.Path:
+    """
+    Create Image with map overlay from APT file, also generate composite images
+    :param apt_filename: path to APT file
+    :param config: Config file path or dict/json object
+    :return: Result directory path
+    """
+    if isinstance(config, (str, pathlib.Path)):
+        config = json.load(open(config))
+
+    apt_filename = pathlib.Path(apt_filename)
+    result_dir = apt_filename.parent / apt_filename.stem
+    result_dir.mkdir(parents=True, exist_ok=True)
+    ret_fn = result_dir / (apt_filename.stem + '.png')
+
+    apt = Apt.from_apt(apt_filename)
+
+    if apt.process():
+        sys.exit(1)
+
+    img = Image.fromarray((apt.data * 255).clip(0, 255).astype(np.uint8), 'L')
+    img.save(ret_fn, 'png')
+
+    msh = MapShapes(config)
+    apt.create_maps_overlay(msh)
+
+    map_overlay = apt.map_overlay
+    map_overlay = Image.fromarray(map_overlay, 'RGBA')
+
+    img = img.convert('RGB')
+    img.paste(map_overlay, (apt.IMAGE_A_START, 0), map_overlay)
+    img.paste(map_overlay, (apt.IMAGE_B_START, 0), map_overlay)
+    img.save(ret_fn.with_stem(ret_fn.stem + '_map'), 'png')
+
+    comps = 'HVC', 'HVCT', 'MSA', 'NO', 'HVCP', 'HVCTP', 'MSAP', 'SEA', 'THRM', 'BP'
+    for c, img in apt.create_composites(*comps):
+        img.save(ret_fn.with_stem(f'{ret_fn.stem}_{c}'), 'png')
+
+    return result_dir
