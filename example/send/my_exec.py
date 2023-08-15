daed28d1ce28fbfd949151e6242fc7b14320eeb5
baskiton <baskiton@mail.ru>
2023-08-15 16:15:16 +0700

    add example of own executor and server to receive data from executor

---
+++ example/send/my_exec.py
@@ -0,0 +1,283 @@
+import atexit
+import datetime as dt
+import dateutil.tz
+import json
+import logging
+import logging.handlers
+import multiprocessing as mp
+import os
+import pathlib
+import select
+import socket as sk
+import threading
+import time
+
+from collections import deque
+
+from sats_receiver import utils, HOMEDIR
+
+
+SENDF = HOMEDIR / 'send.json'
+SERVER = 'deb', 7373
+
+
+class Sender(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.log = logging.getLogger('Sender')
+
+        try:
+            SENDF.touch(exist_ok=False)
+            SENDF.write_text('{\n}')
+        except FileExistsError:
+            pass
+
+        self.lock = threading.Lock()
+        self.data = json.load(SENDF.open('r+'))
+        self.q = deque(self.data)
+        self._stop_rd, self._stop_wr = os.pipe()
+        self.err_t_conn = 0
+        self.err_dt_conn = 5
+
+    def stop(self):
+        os.write(self._stop_wr, b'.')
+
+    def push(self, **data):
+        with self.lock:
+            key = pathlib.Path(data['filename']).name
+            self.data.update({key: data})
+            json.dump(self.data, SENDF.open('w'))
+            self.q.append(key)
+
+    def send(self, data):
+        fp = pathlib.Path(data['filename'])
+
+        with sk.socket(sk.AF_INET, sk.SOCK_STREAM) as s:
+            s.connect(SERVER)
+
+            data['fsize'] = fp.stat().st_size
+            data['filename'] = fp.name
+
+            self.log.debug('send `%s`', fp.name)
+
+            d_raw = json.dumps(data).encode()
+            d_len = len(d_raw).to_bytes(4, 'little', signed=False)
+            s.send(d_len)
+            s.send(d_raw)
+
+            while 1:
+                ret = s.recv(4096)
+                if ret == b'RDY':
+                    s.sendfile(fp.open('rb'))
+
+                else:
+                    break
+
+        self.log.debug('send `%s` done', fp.name)
+
+    def run(self):
+        poller = select.poll()
+        poller.register(self._stop_rd, select.POLLIN)
+
+        while 1:
+            if poller.poll(5000):
+                break
+
+            while self.q:
+                flush = 0
+                fn = self.q[0]
+                data = self.data.get(fn)
+                if not data:
+                    self.log.warning('No data for %s. Skip', fn)
+
+                else:
+                    try:
+                        self.send(data)
+                        self.err_dt_conn = 5
+                        flush = 1
+
+                    except ConnectionError as e:
+                        t = time.monotonic()
+                        if t > self.err_t_conn:
+                            self.err_t_conn = t + self.err_dt_conn
+                            self.err_dt_conn *= 2
+                            self.log.warning('send: %s', e.strerror)
+                        break
+
+                    except OSError as e:
+                        self.log.error('send: %s', e)
+                        flush = 1
+
+                    except:
+                        self.log.exception('send')
+
+                with self.lock:
+                    if flush:
+                        self.data.pop(fn)
+                        json.dump(self.data, SENDF.open('w'))
+                    self.q.popleft()
+
+
+class Executor(mp.Process):
+    def __init__(self, q: mp.Queue = None, sysu_intv=utils.SysUsage.DEFAULT_INTV):
+        super().__init__(daemon=True, name=self.__class__.__name__)
+
+        self.q = q
+        self.sysu_intv = sysu_intv
+        self.rd, self.wr = mp.Pipe(False)
+
+    def _setup_process(self):
+        logger = logging.getLogger()
+        logger.handlers.clear()
+        logger.setLevel(mp.get_logger().level)
+        if self.q is not None:
+            qh = logging.handlers.QueueHandler(self.q)
+            logger.addHandler(qh)
+            # logging.basicConfig(level=mp.get_logger().level, handlers=[qh])
+        self.log = logging.getLogger(self.name)
+        self.sysu = utils.SysUsage(self.name, self.sysu_intv)
+
+        self.sender = Sender()
+        self.sender.start()
+        atexit.register(lambda x: x.stop(), self.sender)
+
+    def start(self) -> None:
+        super(Executor, self).start()
+        self.rd.close()
+
+    def run(self):
+        self._setup_process()
+
+        self.log.debug('start')
+
+        try:
+            self.action()
+        except:
+            self.log.exception('Exception:')
+        finally:
+            try:
+                self.sender.stop()
+                self.stop()
+            except:
+                self.log.exception('stop Exception:')
+
+        self.log.debug('finish')
+
+    def action(self):
+        while self.sender.is_alive():
+            self.sysu.collect()
+
+            try:
+                x = self.rd.poll(1)
+            except InterruptedError:
+                x = 1
+            except:
+                continue
+
+            if not x:
+                continue
+
+            x = self.rd.recv()
+
+            if x == '.':
+                break
+
+            try:
+                fn, args, kwargs = x
+            except ValueError:
+                self.log.error('invalid task: %s', x)
+                continue
+
+            if callable(fn):
+                try:
+                    x = fn(*args, **kwargs)
+                except Exception:
+                    self.log.exception('%s with args=%s kwargs=%s', fn, args, kwargs)
+                    continue
+
+                if not x:
+                    continue
+
+                decoder_type = x[0]
+                dty = decoder_type.value
+
+                if decoder_type in (utils.Decode.RAW, utils.Decode.CSOFT, utils.Decode.APT):
+                    _, sat_name, fin_key, res_filename, end_time = x
+                    self.sender.push(decoder_type=dty,
+                                     sat_name=sat_name,
+                                     fin_key=fin_key,
+                                     filename=str(res_filename),
+                                     end_time=str(end_time))
+
+                elif decoder_type == utils.Decode.SSTV:
+                    _, sat_name, fin_key, fn_dt = x
+                    for fn, end_time in fn_dt:
+                        self.sender.push(decoder_type=dty,
+                                         sat_name=sat_name,
+                                         fin_key=fin_key,
+                                         filename=str(fn),
+                                         end_time=str(end_time))
+
+                elif decoder_type == utils.Decode.SATS:
+                    _, sat_name, fin_key, files = x
+                    for ty_, files_ in files:
+                        for fn in files_:
+                            self.sender.push(decoder_type=dty,
+                                             sat_name=sat_name,
+                                             fin_key=fin_key,
+                                             filename=str(fn),
+                                             end_time=str(dt.datetime.fromtimestamp(
+                                                 fn.stat().st_mtime, dateutil.tz.tzutc())))
+
+    def execute(self, fn, *args, **kwargs):
+        if self.wr:
+            self.wr.send((fn, args, kwargs))
+
+    def stop(self):
+        if self.wr:
+            self.wr.send('.')
+            utils.close(self.wr)
+            self.wr = 0
+
+
+if __name__ == '__main__':
+    from sats_receiver.async_signal import AsyncSignal
+
+    def setup_logging(q: mp.Queue, log_lvl: int):
+        if not isinstance(log_lvl, int):
+            raise ValueError('Invalid log level: %s' % log_lvl)
+
+        logger = logging.getLogger()
+        logger.setLevel(log_lvl)
+        logger.addHandler(logging.handlers.QueueHandler(q))
+        mp.get_logger().setLevel(log_lvl)
+
+        fmt = logging.Formatter('%(asctime)s %(levelname)s: %(name)s: %(message)s')
+        sh = logging.StreamHandler()
+        sh.setFormatter(fmt)
+
+        qhl = logging.handlers.QueueListener(q, sh)
+        qhl.start()
+        atexit.register(qhl.stop)
+
+    q = mp.Queue()
+    setup_logging(q, logging.DEBUG)
+
+    logging.info('Hello!')
+
+    asig = AsyncSignal(['SIGABRT', 'SIGHUP', 'SIGINT', 'SIGTERM', 'SIGUSR1', 'SIGUSR2', 'SIGBREAK'])
+    ee = Executor(q)
+    ee.start()
+    atexit.register(lambda x: (x.stop(), x.join()), ee)
+
+    while ee.is_alive():
+        signame = asig.wait(1)
+        if signame:
+            if 'USR' in signame:
+                # TODO
+                pass
+            else:
+                logging.info('Exit by %s', signame)
+                break
+
+    logging.info('Bye!')
---
+++ example/send/recv_server.py
@@ -0,0 +1,305 @@
+import atexit
+import io
+import json
+import logging
+import logging.handlers
+import multiprocessing as mp
+import os
+import pathlib
+import pprint
+import sys
+import socketserver as ss
+import subprocess as sp
+import threading
+import time
+
+import numpy as np
+
+from PIL import Image
+from sats_receiver.async_signal import AsyncSignal
+from sats_receiver.systems.apt import Apt
+from sats_receiver.utils import Decode, MapShapes, numbi_disp, close
+
+
+RECV_PATH = pathlib.Path('/media/MORE/sats_receiver/records')
+MAP_SHAPES = pathlib.Path('~/PycharmProjects/sats-receiver/map_shapes.json').expanduser().absolute()
+SATDUMP = pathlib.Path('~/CLionProjects/satdump/build/satdump').expanduser().absolute()
+SD_METEOR_72 = 'meteor_m2-x_lrpt'
+SD_METEOR_80 = 'meteor_m2-x_lrpt_80k'
+
+
+class Worker(mp.Process):
+    @staticmethod
+    def apt_to_png_map(apt_filename, config):
+        if isinstance(config, (str, pathlib.Path)):
+            config = json.load(open(config))
+
+        apt_filename = pathlib.Path(apt_filename)
+        result_dir = apt_filename.parent / apt_filename.stem
+        result_dir.mkdir(parents=True, exist_ok=True)
+        # ret_fn = result_dir / (apt_filename.stem + '.png')
+        apt_filename = apt_filename.rename(result_dir / apt_filename.name)
+        ret_fn = apt_filename.with_suffix('.png')
+
+        apt = Apt.from_apt(apt_filename)
+
+        if apt.process():
+            sys.exit(1)
+
+        img = Image.fromarray((apt.data * 255).clip(0, 255).astype(np.uint8), 'L')
+        img.save(ret_fn, 'png')
+        os.utime(ret_fn, (apt.end_time.timestamp(), apt.end_time.timestamp()))
+
+        msh = MapShapes(config)
+        apt.create_maps_overlay(msh)
+
+        map_overlay = apt.map_overlay
+        map_overlay = Image.fromarray(map_overlay, 'RGBA')
+
+        img = img.convert('RGB')
+        img.paste(map_overlay, (apt.IMAGE_A_START, 0), map_overlay)
+        img.paste(map_overlay, (apt.IMAGE_B_START, 0), map_overlay)
+        img.save(ret_fn.with_stem(ret_fn.stem + '_map'), 'png')
+
+        # comps = 'HVC', 'HVCT', 'MSA', 'NO', 'HVCP', 'HVCTP', 'MSAP', 'SEA', 'THRM', 'BP'
+        comps = 'HVC', 'HVCT', 'MSA', 'NO', 'HVCTP', 'SEA', 'THRM', 'BP'
+        for c, img in apt.create_composites(*comps):
+            img.save(ret_fn.with_stem(f'{ret_fn.stem}_{c}'), 'png')
+
+        return ret_fn
+
+    def __init__(self, q):
+        super().__init__()
+
+        self.q = q
+        self.rd, self.wr = mp.Pipe(False)
+
+    def put(self, params, fp, dtype):
+        if self.wr:
+            self.wr.send((params, fp, dtype))
+
+    def stop(self):
+        if self.wr:
+            self.wr.send('.')
+            close(self.wr)
+            self.wr = 0
+
+    def _setup_process(self):
+        logger = logging.getLogger()
+        logger.handlers.clear()
+        logger.setLevel(mp.get_logger().level)
+        if self.q is not None:
+            qh = logging.handlers.QueueHandler(self.q)
+            logger.addHandler(qh)
+        self.log = logging.getLogger(self.name)
+
+    def start(self) -> None:
+        super(Worker, self).start()
+        self.rd.close()
+
+    def run(self):
+        self._setup_process()
+
+        self.log.debug('start')
+
+        try:
+            self.action()
+        except:
+            self.log.exception('Exception:')
+        finally:
+            try:
+                self.stop()
+            except:
+                self.log.exception('stop Exception:')
+
+        self.log.debug('finish')
+
+    def action(self):
+        while 1:
+            try:
+                x = self.rd.poll(1)
+            except InterruptedError:
+                x = 1
+            except:
+                continue
+
+            if not x:
+                continue
+
+            x = self.rd.recv()
+
+            if x == '.':
+                break
+
+            try:
+                params, fp, dtype = x
+                res = fp
+            except ValueError:
+                self.log.error('invalid task: %s', x)
+                continue
+
+            self.log.info('%s process...', params['sat_name'])
+
+            if dtype == Decode.CSOFT:
+                res = fp.parent / fp.stem
+                res.mkdir(parents=True, exist_ok=True)
+                sp.run([SATDUMP, SD_METEOR_80 if fp.stem.endswith('80k') else SD_METEOR_72, 'soft', str(fp), res],
+                       cwd=SATDUMP.parent)
+
+            elif dtype == Decode.APT:
+                res = self.apt_to_png_map(fp, MAP_SHAPES)
+
+            self.log.info('%s done: %s', params['sat_name'], res.name)
+
+
+class MyTCPHandler(ss.StreamRequestHandler):
+    @staticmethod
+    def apt_to_png_map(apt_filename, config):
+        if isinstance(config, (str, pathlib.Path)):
+            config = json.load(open(config))
+
+        apt_filename = pathlib.Path(apt_filename)
+        result_dir = apt_filename.parent / apt_filename.stem
+        result_dir.mkdir(parents=True, exist_ok=True)
+        # ret_fn = result_dir / (apt_filename.stem + '.png')
+        apt_filename = apt_filename.rename(result_dir / apt_filename.name)
+        ret_fn = apt_filename.with_suffix('.png')
+
+        apt = Apt.from_apt(apt_filename)
+
+        if apt.process():
+            sys.exit(1)
+
+        img = Image.fromarray((apt.data * 255).clip(0, 255).astype(np.uint8), 'L')
+        img.save(ret_fn, 'png')
+        os.utime(ret_fn, (apt.end_time.timestamp(), apt.end_time.timestamp()))
+
+        msh = MapShapes(config)
+        apt.create_maps_overlay(msh)
+
+        map_overlay = apt.map_overlay
+        map_overlay = Image.fromarray(map_overlay, 'RGBA')
+
+        img = img.convert('RGB')
+        img.paste(map_overlay, (apt.IMAGE_A_START, 0), map_overlay)
+        img.paste(map_overlay, (apt.IMAGE_B_START, 0), map_overlay)
+        img.save(ret_fn.with_stem(ret_fn.stem + '_map'), 'png')
+
+        # comps = 'HVC', 'HVCT', 'MSA', 'NO', 'HVCP', 'HVCTP', 'MSAP', 'SEA', 'THRM', 'BP'
+        comps = 'HVC', 'HVCT', 'MSA', 'NO', 'HVCTP', 'SEA', 'THRM', 'BP'
+        for c, img in apt.create_composites(*comps):
+            img.save(ret_fn.with_stem(f'{ret_fn.stem}_{c}'), 'png')
+
+        return ret_fn
+
+    def __init__(self, request, client_address, server):
+        self.log = logging.getLogger('Handler')
+        super().__init__(request, client_address, server)
+
+    def recv_file(self, fp: pathlib.Path, fsz):
+        numfsz = numbi_disp(fsz)
+        sz_left = fsz
+        t = time.monotonic()
+        t_next = t + 1
+        with fp.open('wb') as f:
+            while sz_left:
+                data = self.request.recv(4096)
+                if not data:
+                    raise ConnectionError('Connection lost')
+                sz_left -= len(data)
+                f.write(data)
+
+                t = time.monotonic()
+                if t > t_next:
+                    t_next = t + 1
+                    self.log.debug('%s %s/%s', fp.name, numbi_disp(fsz - sz_left), numfsz)
+
+        self.log.debug('%s %s/%s', fp.name, numbi_disp(fsz - sz_left), numfsz)
+
+    def handle(self):
+        try:
+            self._handle()
+        except Exception:
+            self.log.error('_handle', exc_info=True)
+        finally:
+            self.request.send(b'END')
+
+    def _handle(self):
+        l = int.from_bytes(self.request.recv(4), 'little', signed=False)
+        params = json.loads(self.request.recv(l).decode())
+
+        out_dir = RECV_PATH / params['sat_name']
+        out_dir.mkdir(parents=True, exist_ok=True)
+
+        try:
+            dtype = Decode(params['decoder_type'])
+        except ValueError:
+            self.log.warning('Invalid dtype. Skip')
+            return
+
+        self.log.info('Receiving %s for %s', dtype, params['sat_name'])
+
+        s = io.StringIO()
+        s.write('\n')
+        pprint.pprint(params, stream=s)
+        s.seek(0)
+        self.log.debug(s.read())
+
+        self.request.send(b'RDY')
+
+        fp = out_dir / params['filename']
+        self.recv_file(fp, params['fsize'])
+
+        self.server.worker.put(params, fp, dtype)
+
+
+def setup_logging(q, log_lvl):
+    if not isinstance(log_lvl, int):
+        raise ValueError('Invalid log level: %s' % log_lvl)
+
+    logger = logging.getLogger()
+    logger.setLevel(log_lvl)
+    logger.addHandler(logging.handlers.QueueHandler(q))
+    mp.get_logger().setLevel(log_lvl)
+
+    fmt = logging.Formatter('%(asctime)s %(levelname)s: %(name)s: %(message)s')
+    sh = logging.StreamHandler()
+    sh.setFormatter(fmt)
+
+    qhl = logging.handlers.QueueListener(q, sh)
+    qhl.start()
+    atexit.register(qhl.stop)
+
+    # PIL logging level
+    pil_logger = logging.getLogger('PIL')
+    pil_logger.setLevel(logging.DEBUG + 2)
+
+
+if __name__ == '__main__':
+    q = mp.Queue()
+    setup_logging(q, logging.DEBUG)
+    logging.info('Hello!')
+
+    asig = AsyncSignal(['SIGABRT', 'SIGHUP', 'SIGINT', 'SIGTERM', 'SIGUSR1', 'SIGUSR2', 'SIGBREAK'])
+    srv = ss.TCPServer(('0.0.0.0', 7373), MyTCPHandler)
+    srv.worker = Worker(q)
+    srv.worker.start()
+    atexit.register(lambda x: (x.stop(), x.join()), srv.worker)
+
+    srv_thr = threading.Thread(target=srv.serve_forever)
+    srv_thr.start()
+    atexit.register(lambda x: (srv.shutdown(), x.join()), srv_thr)
+
+    while srv_thr.is_alive() and srv.worker.is_alive():
+        signame = asig.wait(1)
+        if signame:
+            if 'USR' in signame:
+                # TODO
+                pass
+            else:
+                srv.shutdown()
+                srv.worker.stop()
+                logging.info('Exit by %s', signame)
+                break
+
+    logging.info('Bye!')
